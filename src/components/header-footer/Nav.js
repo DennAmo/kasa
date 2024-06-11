59eb9c533080a5e661976efaeec6a358e2aa2783
Dennis Aubry <dennisaubryy.gmail.com>
2024-06-11 19:58:36 +0200

    refix

--- src/components/header-footer/Nav.js
+++ src/components/header-footer/Nav.js
@@ -4,11 +4,11 @@ import { NavLink } from "react-router-dom";
 function Nav() {
   return (
     <nav className="nav">
-      <NavLink to="/accueil" end className="nav__link" activeclassname="active">
+      <NavLink to="/accueil" end className="nav__link" >
         Accueil
       </NavLink>
 
-      <NavLink to="/about" end className="nav__link" activeclassname="active">
+      <NavLink to="/about" end className="nav__link" >
         A Propos
       </NavLink>
     </nav>
